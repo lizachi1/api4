1defccb5cea9e3aec3edfeb0d7cf14e36c6a5eec
lizachi1 <102459926+lizachi1@users.noreply.github.com>
2024-01-08 17:22:27 +0700

    Add files via upload

---
+++ SignalR/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR;
+
+public class ChatHub : Hub
+{
+    public async Task Send(string message, string userName)
+    {
+        float first = float.Parse(message);
+        float second = float.Parse(userName);
+        await Clients.All.SendAsync("Receive", first + second, first * second);
+    }
+}
---
+++ SignalR/Program.cs
@@ -0,0 +1,14 @@
+using SignalR;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapHub<ChatHub>("/chat");
+
+app.Run();
